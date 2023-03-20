fd128543221e684f8c1888b3bcf3af2c8d3b01d8
Gian Luca Nediani <gianluca.nediani@studio.unibo.it>
2023-03-20 11:47:27 +0100

    completed ex 1a

--- src/main/scala/u03/Lists.scala
+++ src/main/scala/u03/Lists.scala
@@ -21,6 +21,11 @@ object Lists extends App :
       case Cons(h, t) if pred(h) => Cons(h, filter(t)(pred))
       case Cons(_, t) => filter(t)(pred)
       case Nil() => Nil()
+    // Svolto con Pirazzoli
+    def drop[A](l: List[A], n: Int): List[A] = (l, n) match
+      case (Nil(), n) => Nil()
+      case (Cons(h, t), 1) => t
+      case (Cons(h, t), n) => drop(t, n-1)
 
   val l = List.Cons(10, List.Cons(20, List.Cons(30, List.Nil())))
   println(List.sum(l)) // 60
--- src/test/scala/u03/ListTest.scala
+++ src/test/scala/u03/ListTest.scala
@@ -20,3 +20,8 @@ class ListTest:
   @Test def testFilter() =
     assertEquals(Cons(20, Cons(30, Nil())), filter(l)(_>=20))
     assertEquals(Cons(10, Cons(30, Nil())), filter(l)(_!=20))
+
+  @Test def testDrop() =
+    assertEquals(Cons (20 , Cons (30 , Nil ())), drop(l, 1))
+    assertEquals(Cons (30 , Nil ()), drop(l, 2))
+    assertEquals(Nil(), drop(l, 5))
