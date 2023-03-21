d1cf21e1fd5611cdd29cbc67880a78587218356e
Gian Luca Nediani <gianluca.nediani@studio.unibo.it>
2023-03-21 18:56:31 +0100

    completed ex 1d and 2

--- src/main/scala/u03/Lists.scala
+++ src/main/scala/u03/Lists.scala
@@ -15,14 +15,6 @@ object Lists extends App :
       case Cons(h, t) => h + sum(t)
       case _ => 0
 
-    def map[A, B](l: List[A])(mapper: A => B): List[B] = l match
-      case Cons(h, t) => Cons(mapper(h), map(t)(mapper))
-      case Nil() => Nil()
-
-    def filter[A](l1: List[A])(pred: A => Boolean): List[A] = l1 match
-      case Cons(h, t) if pred(h) => Cons(h, filter(t)(pred))
-      case Cons(_, t) => filter(t)(pred)
-      case Nil() => Nil()
     // Svolto con Pirazzoli
     @tailrec
     def drop[A](l: List[A], n: Int): List[A] = (l, n) match
@@ -30,12 +22,33 @@ object Lists extends App :
       case (Cons(_, t), 1) => t
       case (Cons(_, t), n) => drop(t, n-1)
 
+    // Svolto con Pirazzoli
     def append[A](left: List[A], right: List[A]): List[A] = (left, right) match
       case (_, Nil()) => left
       case (Nil(), _) => right
       case (Cons(h, Nil()), right) => Cons(h, right)
       case (Cons(h1, t1), Cons(h2, t2)) => Cons(h1, append(t1, Cons(h2, t2)))
-      
+
+    // Svolto con Pirazzoli
+    def flatMap[A,B](l: List[A])(f: A => List[B]): List[B] = l match
+      case Nil() => Nil()
+      case Cons(h, t) => append(f(h), flatMap(t)(f))
+
+    def map[A, B](l: List[A])(mapper: A => B): List[B] = l match
+      case Cons(_, _) => flatMap(l)(e => Cons(mapper(e), Nil()))
+      case _ => Nil()
+
+    def filter[A](l1: List[A])(pred: A => Boolean): List[A] = l1 match
+      case Cons(h, _) => flatMap(l1)(e => if pred(e) then Cons(e, Nil()) else Nil())
+      case _ => Nil()
+
+    def max(l: List[Int]): Option[Int] = l match
+      case Cons(h, Nil()) => Some(h)
+      case Cons(h1, Cons(h2, t)) if h1 < h2 => max(Cons(h2, t))
+      case Cons(h1, Cons(h2, t)) if h1 > h2 => max(Cons(h1, t))
+      case _ => None
+
+
   val l = List.Cons(10, List.Cons(20, List.Cons(30, List.Nil())))
   println(List.sum(l)) // 60
 
--- src/test/scala/u03/ListTest.scala
+++ src/test/scala/u03/ListTest.scala
@@ -4,31 +4,42 @@ import org.junit.*
 import org.junit.Assert.*
 import Lists.*
 
+
+import scala.Option
+
 class ListTest:
   import List.*
 
   val l: List[Int] = Cons(10, Cons(20, Cons(30, Nil())))
 
-  @Test def testSum() =
+  @Test def testSum(): Unit =
     assertEquals(0, sum(Nil()))
     assertEquals(60, sum(l))
 
-  @Test def testMap() =
+  @Test def testMap(): Unit =
     assertEquals(Cons(11, Cons(21, Cons(31, Nil()))), map(l)(_+1))
     assertEquals(Cons("10", Cons("20", Cons("30", Nil()))), map(l)(_+""))
 
-  @Test def testFilter() =
+  @Test def testFilter(): Unit =
     assertEquals(Cons(20, Cons(30, Nil())), filter(l)(_>=20))
     assertEquals(Cons(10, Cons(30, Nil())), filter(l)(_!=20))
   
-  @Test def testDrop() =
+  @Test def testDrop(): Unit =
     assertEquals(Cons (20 , Cons (30 , Nil ())), drop(l, 1))
     assertEquals(Cons (30 , Nil ()), drop(l, 2))
     assertEquals(Nil(), drop(l, 5))
 
-  @Test def testAppend() =
+  @Test def testAppend(): Unit =
     val tail = Cons (40 , Nil () )
     assertEquals(Cons(10, Cons(20, Cons(30, Cons(40, Nil())))), append(l, tail))
 
-  // @Test def testFlatmap() =
-    
+  @Test def testFlatmap(): Unit =
+    assertEquals(Cons(11, Cons(21, Cons(31, Nil()))), flatMap(l)(v => Cons(v + 1, Nil())))
+    assertEquals(Cons (11, Cons(12, Cons(21, Cons(22, Cons(31, Cons(32, Nil())))))),
+      flatMap(l)(v => Cons(v + 1, Cons(v + 2, Nil()))))
+
+  @Test def testMax(): Unit =
+    assertEquals(Some(25), max(Cons(10, Cons(25, Cons(20, Nil())))))
+    assertEquals(Some(30), max(Cons(10, Cons(20, Cons(30, Nil())))))
+    assertEquals(None, max(Nil()))
+
